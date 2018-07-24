3404731e7d8227be03d5bfab5d2602095147be43
NeeleshGaur <Neeleshgaur.manit@yahoo.com>
2018-07-24 19:27:05 +0530

    commit

--- src/app/child-info/child-info.component.ts
+++ src/app/child-info/child-info.component.ts
@@ -8,7 +8,9 @@ import { childInfoClass } from '../childInfo';
 })
 export class ChildInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // Activate Carousel
+}
 
   ngOnInit() {
   }
@@ -30,6 +32,38 @@ childClass:5,
 childTotalFee:12000,
 childFeePaid:10000,
 childPhoto:'assets/images/2.JPG'
-}
+},
+ {
+ childName:'Ramesh',
+ childAge:12,
+ childClass:6,
+ childTotalFee:12000,
+ childFeePaid:7000,
+ childPhoto:'assets/images/3.JPG'
+ },
+  {
+  childName:'Mohsin',
+  childAge:10,
+  childClass:5,
+  childTotalFee:12000,
+  childFeePaid:7500,
+  childPhoto:'assets/images/4.JPG'
+  },
+   {
+   childName:'Chintu',
+   childAge:15,
+   childClass:10,
+   childTotalFee:12000,
+   childFeePaid:10000,
+   childPhoto:'assets/images/5.JPG'
+   },
+    {
+    childName:'Ramesh',
+    childAge:10,
+    childClass:5,
+    childTotalFee:12000,
+    childFeePaid:10000,
+    childPhoto:'assets/images/6.JPG'
+    }
 ];
 };
